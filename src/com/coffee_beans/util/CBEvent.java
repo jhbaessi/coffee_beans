fdc36f067bb5e601e82d9243e935870007f070ff
jhbae <jhbae@vadas.co.kr>
2016-12-04 19:22:10 +0900

    Add CBEvent’s constructor, it has parameter of byte array.

--- src/com/coffee_beans/util/CBEvent.java
+++ src/com/coffee_beans/util/CBEvent.java
@@ -6,17 +6,29 @@ public class CBEvent extends EventObject {
 	private static final long serialVersionUID = -5572054785385399216L;
 
 	public enum Events {
-		MOVE_TO_SIGNUP_PAGE;
+		MOVE_TO_SIGNUP_PAGE,
+		
+		VERIFY_ACCOUNT;
 	}
 	
-	private Events event;
+	private Events	event;
+	private byte[]	bytes;
 	
-	public CBEvent(Object source, Events event) {
+	public CBEvent(Object source, Events event, byte[] bytes) {
 		super(source);
 		this.event = event;
+		this.bytes = bytes;
+	}
+	
+	public CBEvent(Object source, Events event) {
+		this(source, event, null);
 	}
 	
 	public Events getEvent() {
 		return event;
+	}
+	
+	public byte[] getData() {
+		return bytes;
 	}
 }
