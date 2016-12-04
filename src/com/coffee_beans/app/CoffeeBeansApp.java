98cbb0f4db504400c9b7ba0870c06f88e68e73cc
jhbae <jhbae@vadas.co.kr>
2016-12-04 19:27:07 +0900

    Modify way of GUI displaying

--- src/com/coffee_beans/app/CoffeeBeansApp.java
+++ src/com/coffee_beans/app/CoffeeBeansApp.java
@@ -1,15 +1,9 @@
 package com.coffee_beans.app;
 
-import javax.swing.JFrame;
-
-import com.coffee_beans.gui.SignInPanel;
+import com.coffee_beans.gui.GuiManager;
 
 public class CoffeeBeansApp {
 	public static void main(String [] args) {
-		JFrame frame = new JFrame();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(960,540);	// 1024x576, 960x540, 864,486
-		
-		frame.setVisible(true);
+		GuiManager guiManager = new GuiManager();
 	}
 }
