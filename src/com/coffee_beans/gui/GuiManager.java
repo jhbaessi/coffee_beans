22f4675108698e3a97fae3ca56c79d0f70d0840f
jhbae <jhbae@vadas.co.kr>
2016-12-04 19:23:51 +0900

    Apply event model into GuiManager and SignInPanel

--- src/com/coffee_beans/gui/GuiManager.java
+++ src/com/coffee_beans/gui/GuiManager.java
@@ -5,7 +5,13 @@ import java.awt.CardLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class GuiManager {
+import com.coffee_beans.common.Account;
+import com.coffee_beans.util.CBEvent;
+import com.coffee_beans.util.CBEvent.Events;
+import com.coffee_beans.util.CBEventListener;
+import com.coffee_beans.util.CBSerializer;
+
+public class GuiManager implements CBEventListener{
 	private final int APP_WINDOW_WIDTH = 960;	// or 1024
 	private final int APP_WINDOW_HEIGHT = 540;	// or 576
 	
@@ -32,6 +38,8 @@ public class GuiManager {
 	public GuiManager() {
 		buildGui();
 		
+		SignInPanel.getInstance().addEventListener(this);
+		
 		setPage(PageIndex.SIGN_IN_PAGE);
 		
 		mainFrame.setVisible(true);
@@ -54,5 +62,23 @@ public class GuiManager {
 	public void setPage(PageIndex page) {
 		CardLayout layout = (CardLayout)mainPanel.getLayout();
 		layout.show(mainPanel, page.toString());
+	}
+
+	@Override
+	public void eventReceived(CBEvent event) {
+		// handle events
+		
+		if (event.getEvent() == Events.MOVE_TO_SIGNUP_PAGE) {
+			setPage(PageIndex.SIGN_UP_PAGE);
+		} else if (event.getEvent() == Events.VERIFY_ACCOUNT) {
+			// pass to event handler
+			Account account = (Account)CBSerializer.deserialize(event.getData());
+			
+			System.out.println("Deserialized data");
+			System.out.println("email: " + account.getEmail());
+			System.out.println("password: " + account.getPassword());
+		} else {
+			System.out.println("Unknown event");
+		}
 	}
 }
