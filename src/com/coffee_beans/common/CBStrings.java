5383ddaa7deec24ceb300e561469ecc6ca78dbe4
jhbae <jhbae@gmail.com>
2016-12-29 19:44:23 +0900

    Add a new panel for update password
    This commit have only about GUI.

--- src/com/coffee_beans/common/CBStrings.java
+++ src/com/coffee_beans/common/CBStrings.java
@@ -3,6 +3,8 @@ package com.coffee_beans.common;
 public enum CBStrings {
 	EMAIL						("Email"),
 	PASSWORD					("Password"),
+	NEW_PASSWORD				("New password"),
+	CONFIRM_PASSWORD			("Confirm password"),
 	SIGN_IN						("Sign in"),
 	SIGN_UP						("Sign up"),
 	FOR_GOT_PASSWORD			("For got password"),
@@ -10,7 +12,8 @@ public enum CBStrings {
 	TERMS_OF_SERVICE			("terms of service"),
 	PRIVACY_POLICY				("privacy policy"),
 	NOTICE_SIGN_UP				("By clicking \"Sign Up\", you agree to our"),
-	REQUEST_ACCOUNT_VERIFICATION("Request for account verification");
+	REQUEST_ACCOUNT_VERIFICATION("Request for account verification"),
+	UPDATE_PASSWORD				("Update password");
 	
 	private final String text;
 	
