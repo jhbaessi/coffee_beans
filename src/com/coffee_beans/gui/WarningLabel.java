4da4102d5ed8dd0155b620eaac3df78cdca810c9
jhbae <jhbae@gmail.com>
2017-01-05 18:21:58 +0900

    Add warning messages in the WarningLabel class

--- src/com/coffee_beans/gui/WarningLabel.java
+++ src/com/coffee_beans/gui/WarningLabel.java
@@ -7,12 +7,16 @@ import javax.swing.JLabel;
 public class WarningLabel extends JLabel {
 
 	public enum WarningStrings{
-		NO_WARNING(" "),
+		NO_WARNING				(" "),
 		
-		INVALID_EMAIL_FORM("Email is invalid taken."),
-		EXISTING_EMAIL("Email is invalid or already taken."),
-		NOT_MATCH_PASSWORD("These passswords don't match"),
-		NOT_FOUND_USERINFO("Sorry, can't find that username and email.");
+		ENTER_EMAIL_ADDRESS		("Please enter an email address."),
+		ENTER_USERNAME			("Please enter your username."),
+		ENTER_NEW_PASSWORD		("Please enter a new password."),
+		ENTER_CONFIRM_PASSWORD	("Please enter a confirmation password."),
+		INVALID_EMAIL_FORM		("Email is invalid taken."),
+		EXISTING_EMAIL			("Email is invalid or already taken."),
+		NOT_MATCH_PASSWORD		("These passswords don't match"),
+		NOT_FOUND_USERINFO		("Sorry, can't find that username and email.");
 		
 		private final String text;
 		
