4ed330e126732b0c0934f077311d5efb90424957
jhbae <jhbae@gmail.com>
2017-01-05 18:20:39 +0900

    Add custom events

--- src/com/coffee_beans/util/CBEvent.java
+++ src/com/coffee_beans/util/CBEvent.java
@@ -6,16 +6,26 @@ public class CBEvent extends EventObject {
 	private static final long serialVersionUID = -5572054785385399216L;
 
 	public enum Events {
+		// page change
+		REQ_SIGNIN_PAGE,
 		REQ_SIGNUP_PAGE,
-		REQ_FIND_PASSWORD_PAGE,
+		REQ_ACCOUNT_CONFIRMATION_PAGE,
+		REQ_PASSWORD_UPDATE_PAGE,
 		REQ_MAIN_PAGE,
-		REQ_VERIFYING_ACCOUNT,
+		
+		// requests to server
+		REQ_VERIFYING_ACCOUNT,		// check up on email and password
+		REQ_VERIFYING_USERINFO,		// check up on username and email
 		REQ_NEW_ACCOUNT,
-		REQ_TERMS_OF_SERVICE_PAGE,
-		REQ_PRIVACY_POLICY_PAGE,
+		REQ_UPDATE_PASSWORD,
 
+		// response from server
 		RES_VERIFYING_ACCOUNT,
 		RES_NEW_ACCOUNT,
+		
+		// etc
+		REQ_TERMS_OF_SERVICE_PAGE,
+		REQ_PRIVACY_POLICY_PAGE,
 	}
 	
 	private Events	event;
