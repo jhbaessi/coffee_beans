1fefb0808d2f653021295a8588f47a9d5c20ab44
jhbae <jhbae@gmail.com>
2017-01-05 18:04:02 +0900

    Add a username field & functions in the Account class

--- src/com/coffee_beans/common/Account.java
+++ src/com/coffee_beans/common/Account.java
@@ -5,16 +5,38 @@ import java.io.Serializable;
 public class Account implements Serializable {
 	private static final long serialVersionUID = 6984635402043818853L;
 	
+	private String username;
 	private String email;
 	private String password;
 	
-	public Account(String email, String password) {
+	public Account(String username, String email, String password) {
+		setUsername(username);
+		setEmail(email);
+		setPassword(password);
+	}
+	
+	public Account() {
+		this(null, null, null);
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setEmail(String email) {
 		this.email = email;
-		this.password = password;
 	}
 	
 	public String getEmail() {
 		return email;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
 	}
 	
 	public String getPassword() {
--- tests/test/com/coffee_beans/util/CBSerializerTest.java
+++ tests/test/com/coffee_beans/util/CBSerializerTest.java
@@ -8,17 +8,19 @@ import com.coffee_beans.common.Account;
 import com.coffee_beans.util.CBSerializer;
 
 public class CBSerializerTest {
+	private static final String username = "testusername";
 	private static final String email = "test@coffeebeans.com";
 	private static final String password = "testpasword1234%^&*";
 	
 	@Test
 	public void testSerializer() {
-		Account sourceAccount = new Account(email, password);
+		Account sourceAccount = new Account(username, email, password);
 		
 		byte[] bytes = CBSerializer.serialize(sourceAccount);
 		
 		Account deserializedAccount = (Account)CBSerializer.deserialize(bytes);
 		
+		assertEquals(username, deserializedAccount.getUsername());
 		assertEquals(email, deserializedAccount.getEmail());
 		assertEquals(password, deserializedAccount.getPassword());
 	}
