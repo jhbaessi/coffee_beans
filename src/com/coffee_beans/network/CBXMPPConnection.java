e9dc0113b5cc596dac81a4a4e9f66b28b732574f
Kim Tae Soo <rnhappysmile@gmail.com>
2017-03-10 14:48:42 +0900

    Modify from void connection() to boolean connection()

--- src/com/coffee_beans/network/CBXMPPConnection.java
+++ src/com/coffee_beans/network/CBXMPPConnection.java
@@ -1,61 +1,79 @@
 package com.coffee_beans.network;
 
 import java.io.IOException;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
 
-import javax.security.cert.CertificateException;
+import javax.net.ssl.SSLSocketFactory;
 
 import org.jivesoftware.smack.AbstractXMPPConnection;
-import org.jivesoftware.smack.SASLAuthentication;
 import org.jivesoftware.smack.SmackException;
 import org.jivesoftware.smack.XMPPException;
-import org.jivesoftware.smack.sasl.SASLMechanism;
-import org.jivesoftware.smack.sasl.javax.SASLDigestMD5Mechanism;
 import org.jivesoftware.smack.tcp.XMPPTCPConnection;
 import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
 
 public class CBXMPPConnection {
 	
-	public static final String USERNAME = "username";
-	public static final String PASSWORD = "password";
+	public static final String USERNAME = "admin";
+	public static final String PASSWORD = "smile";
 	public static final String RESOURCE = "rnhappysmile";
 	public static final String SERVICENAME = "127.0.0.1";
 	public static final int PORT = 5222;
-	private static AbstractXMPPConnection connection;
+	public static AbstractXMPPConnection connection;
 	
-	public void connection() throws SmackException, IOException, XMPPException, UnrecoverableKeyException, KeyManagementException, KeyStoreException, NoSuchAlgorithmException, java.security.cert.CertificateException, CertificateException{
-
-		SASLMechanism mechanism = new SASLDigestMD5Mechanism();
-		SASLAuthentication.registerSASLMechanism(mechanism);
-		SASLAuthentication.blacklistSASLMechanism("SCRAM-SHA-1");  
-		SASLAuthentication.unBlacklistSASLMechanism("DIGEST-MD5");
+	public boolean connection() {
+		boolean isConnected = false;
+		
+//		SASLMechanism mechanism = new SASLDigestMD5Mechanism();
+//		SASLAuthentication.registerSASLMechanism(mechanism);
+//		SASLAuthentication.blacklistSASLMechanism("SCRAM-SHA-1");  
+//		SASLAuthentication.unBlacklistSASLMechanism("DIGEST-MD5");
 		
 		// Create the configuration for this new connection
-		XMPPTCPConnectionConfiguration configBuilder = XMPPTCPConnectionConfiguration.builder()
-		.setUsernameAndPassword(USERNAME, PASSWORD)
-		.setResource(RESOURCE)
-		.setServiceName(SERVICENAME)
-		.setPort(PORT)
-		.setSecurityMode(XMPPTCPConnectionConfiguration.SecurityMode.disabled) // TLS(Transport Layer Security) 해제, TLS를 사요하기 위해서는 SPARK 코드의 LocalPreferences를 사용하면 될 듯
-		//.setSecurityMode(XMPPTCPConnectionConfiguration.SecurityMode.required )
-		.build();
-				
-		connection = new XMPPTCPConnection(configBuilder);
-		// Connect to the server
+		XMPPTCPConnectionConfiguration.Builder configBuilder = XMPPTCPConnectionConfiguration.builder();
+		configBuilder.setSecurityMode(XMPPTCPConnectionConfiguration.SecurityMode.ifpossible); // TLS(Transport Layer Security) 해제, TLS를 사요하기 위해서는 SPARK 코드의 LocalPreferences를 사용하면 될 듯
+		configBuilder.setSocketFactory(SSLSocketFactory.getDefault()); // setSocketFactory를 통해 AssertionError 문제 해결됨 
+		configBuilder.setUsernameAndPassword(USERNAME, PASSWORD);
+		configBuilder.setResource(RESOURCE);
+		configBuilder.setServiceName(SERVICENAME);
+		configBuilder.setPort(PORT);
+		configBuilder.setCompressionEnabled(false);
+		configBuilder.setSendPresence(true);
+		configBuilder.setDebuggerEnabled(true);
+			
+		AbstractXMPPConnection clientConnection = new XMPPTCPConnection(configBuilder.build());
+		if(clientConnection == null) {
+			return false;
+		}
 		
-		connection.connect();
+		clientConnection.setPacketReplyTimeout(10000);
+		isConnected = true;
+		
+		try {
+			clientConnection.connect();
+		} catch (SmackException e) {
+			// TODO Auto-generated catch block
+			isConnected = false;
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			isConnected = false;
+			e.printStackTrace();
+		} catch (XMPPException e) {
+			// TODO Auto-generated catch block
+			isConnected = false;
+			e.printStackTrace();
+		}
 		
 		// Log into the server
 		//connection.login();
 				
 		//System.out.println("connection status: " + connection.isConnected());
+		
+		return isConnected;
 	}
 	
-	public void discoonect(){
+	public boolean discoonect(){
 		CBXMPPConnection.connection.disconnect();
+		return true;
 	}
 	
 	public void XMPPlogin(String name, String password){
